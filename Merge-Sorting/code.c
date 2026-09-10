4d0aa0e5b69fa6cc93fd59236841d2ec3bd91737
Minju Kang <ju2d68@g.skku.edu>
2026-09-10 16:59:32 +0900

    updating

--- Merge-Sorting/code.c
+++ Merge-Sorting/code.c
@@ -9,7 +9,9 @@ typedef struct Node {
 } Node;
 
 // recursive sort
-Node *merge_sort(Node *head) {}
+Node *merge_sort(Node *head) {
+  if (head == NULL || head->next == NULL)
+}
 
 // combines two sorted lists
 Node *merge(Node *a, Node *b) {}
@@ -17,7 +19,15 @@ Node *merge(Node *a, Node *b) {}
 int main() {
   FILE *ofile = fopen("hw1_input.txt", "r");
   if (ofile == NULL) return 1;
-
+  // n nodes
+  int n;
+  fscanf(ofile, "%d", &n);
+  // attributes & find key
+  fgets(ofile, "%s", );
+  // linked list
+  while (fgets(, sizeof(), ofile) != NULL) {
+    // strcspn
+  }
   Node *head = NULL;
 
   head = merge_sort(head);
