85a653e9f8f18b8137b4e1903109eb308b51f25f
Minju Kang <ju2d68@g.skku.edu>
2026-09-10 14:49:12 +0900

    Update code.c

--- Merge-Sorting/code.c
+++ Merge-Sorting/code.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct Node {
-  char data;
+typedef struct Node {
+  char *key;
+  char *data;
   struct Node *next;
-}
+} Node;
 
 // recursive sort
 Node *merge_sort(Node *head) {}
 
 // combines two sorted lists
-Node *merge(Node *a, Node *b) {
-  
-}
+Node *merge(Node *a, Node *b) {}
+
 int main() {
   FILE *file;
   file = fopen("hw1_input.txt", "w");
+
+  return 0;
 }
