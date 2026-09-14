cec0edc87e3327847e7fe8c4577a19ac88923125
Minju Kang <ju2d68@g.skku.edu>
2026-09-14 18:43:14 +0900

    update code (merge_sort function)

--- Merge-Sorting/code.c
+++ Merge-Sorting/code.c
@@ -10,19 +10,50 @@ typedef struct Node {
 
 // combines two sorted lists
 Node *merge(Node *a, Node *b) {
-  
+  Node empty;
+  empty.next = NULL;
+
+  while (a != NULL && b != NULL) {
+    if (strcmp(a->key, b->key) <= 0) {
+
+    }
+    else {
+
+    }
+  }
 }
 
 // recursive sort
 // find the midpoint and split it into two halves
 Node *merge_sort(Node *head) {
+  // the number of Node is 0 or 1 -> already sorted
   if (head == NULL || head->next == NULL) {
-
-    merge_sort();
-    merge_sort();
-
-    merge();
+    return head;
   }
+
+  // find midpoint
+  Node *slow = head;
+  Node *fast = head->next;
+
+  while (fast != NULL && fast->next != NULL) {
+    // slow moves forward by 1 node
+    slow = slow->next;
+    // fast moves forward by 2 nodes
+    fast = fast->next->next;
+    // fast reaches the end, slow points to the midpoint
+  }
+
+  Node *left = head;
+  Node *right = slow->next;
+  // split it into two halves (using NULL)
+  slow->next = NULL;
+
+  // recursive sort
+  left = merge_sort(left);
+  right = merge_sort(right);
+
+  // combines two sorted lists
+  return merge(left, right);
 }
 
 int main() {
@@ -35,12 +66,12 @@ int main() {
   fgets(ofile, "%s", );
   // linked list
   while (fgets(, sizeof(), ofile) != NULL) {
-    // strcspn
   }
   Node *head = NULL;
 
   head = merge_sort(head);
   
+  // write results on file
   FILE *rfile = fopen("hw1_output.txt", "w");
   Node *curr = head;
   while (curr != NULL) {
